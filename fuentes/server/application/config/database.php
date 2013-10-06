7a160c79b35e5b8a217315da6d7b324154f6e950
p431i7o <pablito.federico@gmail.com>
2013-10-05 21:59:51 -0400

    config de base de datos y autoload para que levante la libreria de base de datos

--- fuentes/server/application/config/database.php
+++ fuentes/server/application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'ecopyahu';
+$db['default']['password'] = 'eco123';
+$db['default']['database'] = 'ecodb';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
