bf9449f5b0bf20c5420a20c06761851edc917404
Pablo Fedrico Ruiz Diaz Rios <pablo@darkstar.example.net>
2013-10-25 20:13:34 -0300

    mas modificaciones

--- fuentes/server/llamadaLocal.php
+++ fuentes/server/llamadaLocal.php
@@ -5,5 +5,5 @@
  * Ubicacion:Deberia estar en alguna carpeta privada. Accesible al crontab
  */
 //$url = "http://ecopyahu.simgia.com/denuncias_redes_sociales/insertarDenunciasByTwitter";
-$url = "http://ecopyahu/denuncias_redes_sociales/test";
+  $url = "http://ecopyahu.simgia.com/denuncias_redes_sociales/test";
 echo file_get_contents($url);
