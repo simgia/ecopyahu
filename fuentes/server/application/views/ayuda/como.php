1d7b4c82d17b2194b96cf464b25d5f47d58a033a
bauerpy <bauerpy@gmail.com>
2013-10-26 14:36:49 -0300

    cambios en el texto

--- fuentes/server/application/views/ayuda/como.php
+++ fuentes/server/application/views/ayuda/como.php
@@ -9,7 +9,7 @@ $this->load->view('comunes/cabecera');
         <?php $this->load->view('comunes/menu',array("origen"=>"ayuda"))?>
             <h2>&#191;C&oacute;mo denunciar?</h2><br><br>
             <h3>Utilizando Twitter <i class="fa fa-twitter"></i></h3>
-            <p>Para mayor facilidad ecoPYahu se integra con Twitter, utiliz&aacute;ndolo como medio para realizar las denuncias con el simple uso del hashtag #ecopyahu. El sistema constantemente monitorea la red social en busca de twits, al encontrar uno nuevo con el hastag mencionado lo agrega a la base de datos de ecoPyahu y ya se encuentra publicado. Al mismo tiempo se retwittea en la cuenta @ecopyahu para todos sus seguidores.
+            <p>Para mayor facilidad ecoPYahu se integra con Twitter, utiliz&aacute;ndolo como medio para realizar las denuncias con el simple uso del hashtag #ecopyahu; para poder ubicar el lugar de la denuncia, se recomienda activar la ubicaci&oacute;n antes de postear el tweet. El sistema constantemente monitorea la red social en busca de tweets, al encontrar uno nuevo con el hastag mencionado lo agrega a la base de datos de ecoPYahu y ya se encuentra publicado. Al mismo tiempo se retwittea en la cuenta @ecopyahu para todos sus seguidores.
             <code> Enviar un tweet con el <i>hashtag</i> <b>#ecopyahu</b> </code>
             </p>
 
